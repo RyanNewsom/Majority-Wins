48cc802b785f298ca22c588c325dfde17007b5b8
Will Foster <williamgfoster@gmail.com>
2020-09-24 06:59:00 -0600

    Election schema v1 + sample data

--- src/models/App.ts
+++ src/models/App.ts
@@ -6,3 +6,28 @@ export type Item = {
   id: number;
   title: string;
 };
+
+export type Voter = {
+  id: number;
+  firstName: string;
+  lastName: string;
+  address: string;
+  city: string;
+  state: string;
+  birthDate: string;
+  email: string;
+  phone: string;
+};
+
+export type Question = {
+  content: string;
+  yesCount: number;
+  noCount: number;
+};
+
+export type Election = {
+  id: number;
+  name: string;
+  description: string;
+  questions: Question[];
+};
