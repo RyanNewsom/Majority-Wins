6b6268ee5453d1ecd67d541688426b78b1fa1b2b
Will Foster <williamgfoster@gmail.com>
2020-09-25 07:20:00 -0600

    Some changes to enable a home page w/ no nav bar
    Sorry for all the formatting changes, prettier went ham on some of the files.

--- src/components/MainComponent.tsx
+++ src/components/MainComponent.tsx
@@ -1,11 +1,13 @@
-import React from "react";
-import { Election, ElectionForm, Voter } from "../models/App";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { RegisterVotersComponent } from "./RegisterVotersComponent";
-import { CaptureVotesContainer } from "../containers/CaptureVotesContainer";
-import NavBar from "./NavBar";
-import { CreateElectionComponent } from "./CreateElectionComponent";
-import { ElectionTableComponent } from "./ElectionTableComponent";
+import React from 'react';
+import { Election, ElectionForm, Voter } from '../models/App';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { RegisterVotersComponent } from './RegisterVotersComponent';
+import { CaptureVotesContainer } from '../containers/CaptureVotesContainer';
+import NavBar from './NavBar';
+import { CreateElectionComponent } from './CreateElectionComponent';
+import { ElectionTableComponent } from './ElectionTableComponent';
+import { HomeComponent } from './HomeComponent';
+import { Container, Grid } from '@material-ui/core';
 
 export type MainComponentProps = {
   voters: Voter[];
@@ -19,32 +21,36 @@ export type MainComponentProps = {
 export function MainComponent(props: MainComponentProps) {
   return (
     <Router>
-      <>
-        <NavBar />
-        {/* A <Switch> looks through its children <Route>s and
+      {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/voters">
-            <RegisterVotersComponent
-              voters={props.voters}
-              onAddVoter={props.onAddVoter}
-            />
-          </Route>
-          <Route path="/capturevotes">
-            <CaptureVotesContainer />
-          </Route>
-          <Route path="/elections">
+      <Switch>
+        <Route path="/voters">
+          <NavBar />
+          <RegisterVotersComponent voters={props.voters} onAddVoter={props.onAddVoter} />
+        </Route>
+        <Route path="/capturevotes">
+          <NavBar />
+          <CaptureVotesContainer />
+        </Route>
+        <Route path="/elections">
+          <NavBar />
+          <Container maxWidth="md">
             <ElectionTableComponent
               elections={props.elections}
               expandedElectionId={props.expandedElectionId}
               onExpandElectionRow={props.onExpandElectionRow}
             />
-            <CreateElectionComponent
-              onCreateElection={props.onCreateElection}
-            />
-          </Route>
-        </Switch>
-      </>
+          </Container>
+          <Container maxWidth="md">
+            <CreateElectionComponent onCreateElection={props.onCreateElection} />
+          </Container>
+        </Route>
+        <Route path="/">
+          <Container maxWidth="md">
+            <HomeComponent />
+          </Container>
+        </Route>
+      </Switch>
     </Router>
   );
 }
