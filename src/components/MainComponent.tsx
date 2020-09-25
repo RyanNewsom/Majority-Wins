2aa32fa91575cee91dbd597eef969fbee5815135
Will Foster <williamgfoster@gmail.com>
2020-09-25 10:03:17 -0600

    Enable a home page w/ no nav bar (#23)
    * Some changes to enable a home page w/ no nav bar
    * Added favicon and a site title

--- src/components/MainComponent.tsx
+++ src/components/MainComponent.tsx
@@ -1,12 +1,14 @@
-import React from "react";
-import { Election, ElectionForm, Voter } from "../models/App";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { RegisterVotersComponent } from "./RegisterVotersComponent";
-import { CaptureVotesContainer } from "../containers/CaptureVotesContainer";
-import NavBar from "./NavBar";
-import { CreateElectionComponent } from "./CreateElectionComponent";
-import { TableSorting } from "./RegisteredVotersTableComponent";
-import { ElectionTableComponent } from "./ElectionTableComponent";
+import React from 'react';
+import { Election, ElectionForm, Voter } from '../models/App';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { RegisterVotersComponent } from './RegisterVotersComponent';
+import { CaptureVotesContainer } from '../containers/CaptureVotesContainer';
+import NavBar from './NavBar';
+import { CreateElectionComponent } from './CreateElectionComponent';
+import { TableSorting } from './RegisteredVotersTableComponent';
+import { ElectionTableComponent } from './ElectionTableComponent';
+import { Container } from '@material-ui/core';
+import { HomeComponent } from './HomeComponent';
 
 export type MainComponentProps = {
   voters: Voter[];
@@ -35,52 +37,53 @@ export type MainComponentProps = {
 export function MainComponent(props: MainComponentProps) {
   return (
     <Router>
-      <>
-        <NavBar />
-        {/* A <Switch> looks through its children <Route>s and
+      {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/voters">
-            <RegisterVotersComponent
-              voters={props.voters}
-              registeredVotersRowsPerPage={props.registeredVotersRowsPerPage}
-              registeredVotersSelectedVoters={
-                props.registeredVotersSelectedVoters
-              }
-              registeredVotersTablePage={props.registeredVotersTablePage}
-              registeredVotersTableSort={props.registeredVotersTableSort}
-              registeredVotersTabSelected={props.registerVotersTabSelected}
-              registeredVotersSelectedTab={props.registeredVotersSelectedTab}
-              registeredVotersDeleteVoters={props.registeredVotersDeleteVoters}
-              registeredVotersSortSelected={props.registeredVotersSortSelected}
-              registeredVotersSelected={props.registeredVotersVotersSelected}
-              registeredVotersTablePageUpdated={
-                props.registeredVotersTablePageUpdated
-              }
-              registeredVotersRowsPerPageUpdated={
-                props.registeredVotersRowsPerPageUpdated
-              }
-              registeredVotersRowEdited={props.registeredVotersRowEdited}
-              onAddVoter={props.onAddVoter}
-              onSaveVoter={props.onSaveVoter}
-              registeredVoterBeingEdited={props.registeredVoterBeingEdited}
-            />
-          </Route>
-          <Route path="/capturevotes">
-            <CaptureVotesContainer />
-          </Route>
-          <Route path="/elections">
+      <Switch>
+        <Route path="/voters">
+          <NavBar />
+          <RegisterVotersComponent
+            voters={props.voters}
+            registeredVotersRowsPerPage={props.registeredVotersRowsPerPage}
+            registeredVotersSelectedVoters={props.registeredVotersSelectedVoters}
+            registeredVotersTablePage={props.registeredVotersTablePage}
+            registeredVotersTableSort={props.registeredVotersTableSort}
+            registeredVotersTabSelected={props.registerVotersTabSelected}
+            registeredVotersSelectedTab={props.registeredVotersSelectedTab}
+            registeredVotersDeleteVoters={props.registeredVotersDeleteVoters}
+            registeredVotersSortSelected={props.registeredVotersSortSelected}
+            registeredVotersSelected={props.registeredVotersVotersSelected}
+            registeredVotersTablePageUpdated={props.registeredVotersTablePageUpdated}
+            registeredVotersRowsPerPageUpdated={props.registeredVotersRowsPerPageUpdated}
+            registeredVotersRowEdited={props.registeredVotersRowEdited}
+            onAddVoter={props.onAddVoter}
+            onSaveVoter={props.onSaveVoter}
+            registeredVoterBeingEdited={props.registeredVoterBeingEdited}
+          />
+        </Route>
+        <Route path="/capturevotes">
+          <NavBar />
+          <CaptureVotesContainer />
+        </Route>
+        <Route path="/elections">
+          <NavBar />
+          <Container maxWidth="md">
             <ElectionTableComponent
               elections={props.elections}
               expandedElectionId={props.expandedElectionId}
               onExpandElectionRow={props.onExpandElectionRow}
             />
-            <CreateElectionComponent
-              onCreateElection={props.onCreateElection}
-            />
-          </Route>
-        </Switch>
-      </>
+          </Container>
+          <Container maxWidth="md">
+            <CreateElectionComponent onCreateElection={props.onCreateElection} />
+          </Container>
+        </Route>
+        <Route path="/">
+          <Container maxWidth="md">
+            <HomeComponent />
+          </Container>
+        </Route>
+      </Switch>
     </Router>
   );
 }
